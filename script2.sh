b2fa8ddd6629c4ede3638765e974d034795836ed
Shiningbull <gaetan.babulle@gmail.com>
2018-01-20 01:20:50 +0100

    modification du texte sur le deuxieme script

--- script2.sh
+++ script2.sh
@@ -1,4 +1,4 @@
 
 
 
-echo 'Re salut lami ! Jespere que tu vas toujours aussi bien ou mieux alors... Et bien voila je tebais simplement à te dire au revoir et bon courage si toi aussi tu commences une formation sur Openclassrooms, au plaisir !'
+echo 'Re salut lami ! Jespere que tu vas toujours aussi bien ou mieux alors... Et bien voila je tenais simplement à te dire au revoir et bon courage si toi aussi tu commences une formation sur Openclassrooms, au plaisir !'
